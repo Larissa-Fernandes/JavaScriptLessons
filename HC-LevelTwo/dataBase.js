ccc599c1f711c7236d915007a893e375f8447446
Larissa Fernandes <larissa.afernandes@outlook.com>
2021-07-13 21:23:40 -0300

    add .gitignore and dataBase.js

---
+++ HC-LevelTwo/dataBase.js
@@ -0,0 +1,67 @@
+const Anime = [
+    {
+        id: 1, 
+        name: "Ao Haru Ride", 
+        studio: "Production I.G",
+        genre: "Shoujo",
+        episodes: 12,
+        recommended: true,
+        finished: true
+    },
+    {
+        id: 2,
+        name: "Ouran Koukou Host Club",
+        studio: "Bones",
+        genre: "Shoujo",
+        episodes: 26,
+        recommended: true,
+        finished: true
+    },
+    {
+        id: 3,
+        name: "Gugure! Kokkuri-san",
+        studio: "TMS Entertainment",
+        genre: "Comedy",
+        episodes: 12,
+        recommended: true,
+        finished: true
+    },
+    {
+        id: 4,
+        name: "Shingeki no Kyojin",
+        studio: "Production I.G",
+        genre: "Shounen",
+        episodes: 75,
+        recommended: true,
+        finished: false
+    },
+    {
+        id: 5, 
+        name: "One Piece",
+        studio: "Toei Animation",
+        genre: "Shounen",
+        episodes: 156,
+        recommended: true,
+        finished: false
+    },
+    {
+        id: 6, 
+        name: "Neon Genesis Evangelion",
+        studio: "Gainax",
+        genre: "Mecha",
+        episodes: 26,
+        recommended: false,
+        finished: true
+    },
+    {
+        id: 7,
+        name: "Aquarion Evol",
+        studio: "Satelight",
+        genre: "Mecha",
+        episodes: 26,
+        recommended: false,
+        finished: true
+    }
+]
+
+module.exports = Anime
